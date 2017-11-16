30fe2d9e39f4ceb6d1fa89f240a7faa639a2d822
MGP42 <dki1602@yahoo.de>
2017-11-16 16:03:04 +0100

    bspwm panel update

--- .config/bspwm/panel2/lemonbar.sh
+++ .config/bspwm/panel2/lemonbar.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/bash
 
 . $(dirname $0)/lemonbar_style
@@ -59,18 +60,64 @@ Conky(){
 		
 }
 
+Window(){
+	while true;do
+		echo  "windowname\ %{A4:i3lock:}$(xdotool getwindowfocus getwindowname)%{A4}" >> ${panel_fifo}
+		sleep 1
+	done
+}
+
 ##################################################################################################
 # One Time Execute with extern update ############################################################
 #################################################################################################
 
 Volume(){
-	bash $(dirname $0)/extern_update/Volume
+	echo  "volume\ $(amixer get Master | grep "${snd_cha}" | awk -F'[]%[]' '/%/ {if ($7 == "off") {print "×"} else {printf "%d%", $2}}')">> ${panel_fifo}
 }
 
 Desktop(){
-	echo 123
+	color_old=$color_tray_bg
+	first=1
+	atm=null
+	tmp=$(echo "$(Spacer_right $color_main2_bg $color_main2_fg $color_old) ")
+
+	for i in $(bspc query -D); do
+		if [ "$(bspc query -d -D)" == "$i" ]
+                then
+			if [ $atm == "0" ]
+			then
+				tmp=$(echo "${tmp::-1} $(Spacer_right $color_main1_bg $color_main1_fg $color_main2_bg)")	
+			else
+				tmp=$(echo "$(Spacer_right $color_main1_bg $color_main1_fg $color_old) ")
+			fi
+				tmp=$(echo "$tmp $(bspc query -T -d $i | cut -d'"' -f4) ")
+			atm=1
+		else
+			if [ "$(bspc query -N -n .window -d $i)" != "" ]
+			then
+				if [ $atm == "1" ]
+				then
+					tmp=$(echo "$tmp $(Spacer_right $color_main2_bg $color_main2_fg $color_main1_bg)")
+				fi
+				tmp=$(echo " $tmp $(bspc query -T -d $i | cut -d'"' -f4) $sep_l_right")
+				atm=0
+			fi
+		fi
+	done
+	
+	if [ $atm == "1" ]
+	then
+		tmp=$(echo "$tmp $(Spacer_right $color_tray_bg $color_tray_fg $color_main1_bg)")
+	else
+		tmp=$(echo "${tmp::-1} $(Spacer_right $color_tray_bg $color_tray_fg $color_main2_bg)")
+	fi
+	Fast_Window &
+	echo "desktop\ $tmp" >> ${panel_fifo}
 }
 
+Fast_Window(){
+	echo  "windowname\ %{A4:i3lock:}$(xdotool getwindowfocus getwindowname)%{A4}" >> ${panel_fifo}
+}
 
 ##################################################################################################
 # Parser #########################################################################################
@@ -82,7 +129,7 @@ lemonbar_parser(){				#Parses FiFo to Variables
                 value=$(echo $line | cut -d\\ -f2)
                 case $index in
                         (clock) clock=$value;;
-			(memory) memory="$icon_memory $value";;
+			(memory) memory="$icon_memory$value";;
 			(network) 
 				network="$icon_up $(echo $value | cut -d+ -f1)"
                                 network+=" $sep_l_left "
@@ -95,6 +142,7 @@ lemonbar_parser(){				#Parses FiFo to Variables
                         (volume) volume=$icon_volume$value;;
                         (conky) conky=$value;;
                         (windowname) windowname=$value;;
+			(extern) $($value);;
                 esac
         done < ${panel_fifo}
 
@@ -111,6 +159,13 @@ Spacer_left(){					#Spacer to the left <bgcolor|fgcolor>
         echo -n "%{F#$color_bg}$sep_left%{B#$color_bg}%{F#$color_fg}"
 }
 
+Spacer_right(){					#Spacer to the right <bgcolor|fgcolor|old bgcolor>
+        color_old=$3	#$color_bg	#Bug c_old
+        color_bg=$1
+        color_fg=$2
+        echo "%{F#$color_old}%{B#$color_bg}$sep_right%{F#$color_fg}"
+}
+
 
 
 ##################################################################################################
@@ -122,27 +177,47 @@ Date &
 Battery &
 Conky &
 Volume &
+Window &
+Desktop &
 
 lemonbar_output(){
 	while true; do
 		lemonbar_parser
-		echo -n %{F#$color_tray_fg}%{B#$color_tray_bg}
-
 		# left
-		echo -n $cpu
-		echo -n $network
-		echo -n $memory
-
+		echo -n "          "
+		echo -n "$desktop"
+		echo -n "$windowname"
+		#echo -n " $(Spacer_right $color_main1_bg $color_main1_fg $color_tray_bg) "
+		#echo -n " $(Spacer_right $color_tray_bg $color_tray_fg $color_main1_bg) $windowname "
 		# right
 		echo -n %{r}
-		
+
+		echo -n " $(Spacer_left $color_info1_bg $color_info1_fg) $network"
+		echo -n " $(Spacer_left $color_info2_bg $color_info2_fg) $cpu $sep_l_left $memory"
 		echo -n " $(Spacer_left $color_info1_bg $color_info1_fg)$battery"	#Batetry+Spacer
 		echo -n "$(Spacer_left $color_info2_bg $color_info2_fg) $volume"	#Volume +Spacer
 		echo -n " $(Spacer_left $color_info1_bg $color_info1_fg)$date"		#Date	+Spacer
-		echo " $(Spacer_left $color_main1_bg $color_main1_fg)$clock"		#Clock	+Spacer
+		echo -n " $(Spacer_left $color_main1_bg $color_main1_fg)$clock"		#Clock	+Spacer
+
+		echo %{F#$color_tray_fg}%{B#$color_tray_bg}		#Anti fuck up / do not touch
 	done	
 }
 
 
 lemonbar_output | lemonbar -p -g $geometry  -B "#000000" -F "#ffffff"    -f $font_symbol  -f $font_basic | /bin/bash
 
+
+
+
+
+
+
+##################################################################################################
+# BUGS ###########################################################################################
+##################################################################################################
+#
+#	c_old
+#		color_old not global defined
+#		if global defined then Spacer_right has no need for color_old in the
+#		Constructor
+#			-Fix delayed
