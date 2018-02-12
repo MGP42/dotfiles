d2fc09eb18064867bb76fba0bf202fb40aa21e52
MGP42 <dki1602@yahoo.de>
2018-02-12 15:34:52 +0100

    multi desktop script

--- .config/bspwm/scripts/monitor.sh
+++ .config/bspwm/scripts/monitor.sh
@@ -1,3 +1,12 @@
+for i in $(bspc query -M --names); do
+        if [ "" != "$(xrandr | grep -w $i | grep disconnected)" ];then
+                bspc desktop -f $i:focused
+                bspc monitor --remove
+		bspc desktop -f ^1:focused
+        fi
+done
+
+
 for i in $(xrandr | grep -o "^.* connected" | sed "s/ connected//"); do
 	bspc monitor $i -d I II III IV V VI VII VIII IX X
 done
