6480a5ab739f9c5569b1ab84681f09320cfd3c0c
MGP42 <dki1602@yahoo.de>
2023-12-28 18:41:56 +0100

    clipboard complete

--- .config/polybar/clipboard.sh
+++ .config/polybar/clipboard.sh
@@ -2,14 +2,25 @@
 # Handle copies that do not work in polybar itself
 # Mainly cause by having ':' in the string
 
+wlan=$(nmcli device show | grep  "wifi" -B 1 | head -n 1 | cut -c 41-)
+eth=$(nmcli device show | grep  "ethernet" -B 1 | head -n 1 | cut -c 41-)
+
+
 case $1 in
 wireless-mac)
-    ip addr | grep '[0-9]*: w' -A 3 | grep link | xargs | cut -d " " -f 2 | tr -d '\n' | xclip -selection clipboard
+    ip addr | grep $wlan -A 3 | grep link | xargs | cut -d " " -f 2 | tr -d '\n' | xclip -selection clipboard
     ;;
 wireless-ipv6)
-    ip addr | grep '[0-9]*: w' -A 5 | grep inet6 | xargs | cut -d " " -f 2 | cut -d "/" -f 1 | tr -d '\n' | xclip -selection clipboard
+    ip -6 addr | grep $wlan -A 20 | grep fe80 -B2 | grep inet6 -m1 | xargs | cut -d " " -f 2 | cut -d "/" -f 1 | tr -d '\n' | xclip -selection clipboard
     ;;
 public-ipv4)
     wget -q -O - ipinfo.io/ip | tr -d '\n' | xclip -selection clipboard
+    notify-send public "ipv4 copied"
+    ;;
+ethernet-mac)
+    ip addr | grep $eth -A 3 | grep link | xargs | cut -d " " -f 2 | tr -d '\n' | xclip -selection clipboard
+    ;;
+ethernet-ipv6)
+    ip -6 addr | grep $eth -A 20 | grep fe80 -B2 | grep inet6 -m1 | xargs | cut -d " " -f 2 | cut -d "/" -f 1 | tr -d '\n' | xclip -selection clipboard
     ;;
 esac
