36ced3dfd3bce69ae8bb7c0243d77f13fb38614e
MGP42 <dki1602@yahoo.de>
2024-02-05 08:24:31 +0100

    multi-monitor adjustements and polymusic fix

--- .config/polybar/microphone.sh
+++ .config/polybar/microphone.sh
@@ -2,7 +2,7 @@
 #pacmd list-sources | grep "\* index:" -A 7 | grep front | cut -d '/' -f 2 | xargs
 #pacmd list-sources | grep "\* index:" -A 11 | grep muted | xargs | cut -d " " -f 2
 
-vol=$(pacmd list-sources | grep "\* index:" -A 7 | grep front | cut -d '/' -f 2 | xargs)
+vol=$(pacmd list-sources | grep "\* index:" -A 7 | grep volume | cut -d '/' -f 2 | xargs)
 muted=$(pacmd list-sources | grep "\* index:" -A 11 | grep muted | xargs | cut -d " " -f 2)
 if [[ $muted == "yes" ]]; then
     echo "%{F$1}%{F-}"
@@ -13,7 +13,7 @@ fi
 
 LANG=en_US.UTF-8 pactl subscribe | while read -r event; do
     if [[ "$event" == *"source"* ]]; then
-        vol=$(pacmd list-sources | grep "\* index:" -A 7 | grep front | cut -d '/' -f 2 | xargs)
+        vol=$(pacmd list-sources | grep "\* index:" -A 7 | grep volume | cut -d '/' -f 2 | xargs)
         muted=$(pacmd list-sources | grep "\* index:" -A 11 | grep muted | xargs | cut -d " " -f 2)
         if [[ $muted == "yes" ]]; then
             echo "%{F$1}%{F-}"
