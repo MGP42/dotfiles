c2c2396323d27bf204728a256256be7627e815b9
MGP42 <dki1602@yahoo.de>
2024-02-05 10:04:42 +0100

    i3lock multimonitor and mediakeys for sxhk

--- .config/bspwm/scripts/lock.sh
+++ .config/bspwm/scripts/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # needs https://github.com/Lixxia/i3lock
-i3lock -i ~/Bilder/.tmp/lock_bg.png -o '#191d0f' -w '#572020' -l '#ffffff' -e --24
+i3lock -i ~/Bilder/.tmp/lock_bg.png -o '#191d0f' -w '#572020' -l '#ffffff' -e --24 -t
