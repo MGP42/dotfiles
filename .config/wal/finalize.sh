846ef23b699d031b13b4f507ebfed29fd3976073
dki <dki1602@yahoo.de>
2025-02-26 09:14:20 +0100

    speedcrunch

--- .config/wal/finalize.sh
+++ .config/wal/finalize.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 bash ~/.config/bspwm/bspwm-colors.sh
-
+if [ ! -d ~/.local/share/SpeedCrunch/color-schemes ]; then
+	mkdir -p ~/.local/share/SpeedCrunch/color-schemes
+fi
+cp ~/.cache/wal/colors-speedcrunch.json ~/.local/share/SpeedCrunch/color-schemes/
