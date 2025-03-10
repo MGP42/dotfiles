0cc48fb489e73e51dc6634c1ed7a6b6bb05eadf7
MGP42 <dki1602@yahoo.de>
2025-03-10 11:09:25 +0100

    notifications

--- .config/bspwm/scripts/monitor.sh
+++ .config/bspwm/scripts/monitor.sh
@@ -11,3 +11,5 @@ done
 for i in $(xrandr | grep -o "^.* connected" | sed "s/ connected//"); do
 	bspc monitor $i -d I II III IV V VI VII VIII IX X
 done
+
+notify-send "BSPWM" "Adjusted Desktops to amount of Monitors"
