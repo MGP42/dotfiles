b7842bbebb035258a9f229e5e0834b6416a3b7e4
dki <dki1602@yahoo.de>
2025-03-11 09:54:05 +0100

    pywalfox

--- .config/wal/finalize.sh
+++ .config/wal/finalize.sh
@@ -4,3 +4,4 @@ if [ ! -d ~/.local/share/SpeedCrunch/color-schemes ]; then
 	mkdir -p ~/.local/share/SpeedCrunch/color-schemes
 fi
 cp ~/.cache/wal/colors-speedcrunch.json ~/.local/share/SpeedCrunch/color-schemes/
+pywalfox update
