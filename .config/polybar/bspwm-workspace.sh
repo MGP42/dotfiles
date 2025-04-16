aac791386c0f5eb594f0b3ddb273c1600ed509fe
dki <dki1602@yahoo.de>
2025-04-16 15:56:42 +0200

    local

--- .config/polybar/bspwm-workspace.sh
+++ .config/polybar/bspwm-workspace.sh
@@ -46,7 +46,7 @@ returnData() {
         if ($0 == active)
             printf "%s%s%s", active_start, $0, active_end;
 				else if ($0!="")
-            printf " %s%s%s%s%s ","%{A1:bspc desktop -f ", $0, ":}", $0, "%{A}";
+            printf " %s%s%s%s%s ","%{A1:bspc desktop -f ", $0, ".local:}", $0, "%{A}";
     }
     END {
         print end
